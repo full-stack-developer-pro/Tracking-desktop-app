e624f6c63b7a51084a833cb928dda97e16634313
Full-stack-developer-pro <gurleenkaur58881@gmail.com>
2025-12-10 17:44:44 +0530

    removed alert modal from starting

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.log("Main.tsx: Starting execution");
-// ALERT FOR DEBUGGING PRODUCTION - REMOVE LATER
-window.alert("Main.tsx: Starting execution. If you see this, JS is running!");
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-console.log("Main.tsx: React root rendered");
 
 if (window.ipcRenderer) {
-  console.log("Main.tsx: ipcRenderer found");
   window.ipcRenderer.on("main-process-message", (_event, message) => {
     console.log("Main process message:", message);
   });
